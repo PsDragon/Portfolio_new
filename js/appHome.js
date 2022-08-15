98e79ab223d23a42e7913235f52a3561ee0d0130
PsDragon <210850@buas.nl>
2022-08-15 18:43:02 +0200

    update

--- js/appHome.js
+++ js/appHome.js
@@ -16,7 +16,7 @@ particlesJS('particles-js',
   {
     "particles": {
       "number": {
-        "value": 80,
+        "value": 250,
         "density": {
           "enable": true,
           "value_area": 800
@@ -45,7 +45,7 @@ particlesJS('particles-js',
         "random": false,
         "anim": {
           "enable": false,
-          "speed": 1,
+          "speed": 2,
           "opacity_min": 0.1,
           "sync": false
         }
